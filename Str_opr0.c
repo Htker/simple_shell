3d2660576b1439048ab77803ff06a44c133a5359
SNPooyusuf <yosefosama902@gmail.com>
2023-11-04 02:09:28 -0700

    string functions

---
+++ Str_opr0.c
@@ -0,0 +1,80 @@
+#include "shell.h"
+
+/**
+ * _strlen - Returns the length of a string.
+ * @s: The string to check the length of.
+ *
+ * Return: The length of the string as an integer.
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	if (!s)
+		return (0);
+
+	while (*s)
+	{
+		length++;
+		s++;
+	}
+	return (length);
+}
+
+/**
+ * _strcmp - Compares two strings.
+ * @s1: The first string.
+ * @s2: The second string.
+ *
+ * Return: Negative if s1 < s2, positive if s1 > s2, zero if s1 == s2.
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2)
+	{
+		if (*s1 != *s2)
+			return (*s1 - *s2);
+		s1++;
+		s2++;
+	}
+	if (*s1 == *s2)
+		return (0);
+	else
+		return (*s1 < *s2 ? -1 : 1);
+}
+
+/**
+ * _isstart - Checks if the haystack starts with the needle.
+ * @haystack: The string to search.
+ * @needle: The substring to find.
+ *
+ * Return: Address of the next character of haystack or NULL.
+ */
+char *_isstart(const char *haystack, const char *needle)
+{
+	while (*needle)
+	{
+		if (*needle++ != *haystack++)
+			return (NULL);
+	}
+	return ((char *)haystack);
+}
+
+/**
+ * _strcat - Concatenates two strings.
+ * @dest: The destination buffer.
+ * @src: The source buffer.
+ *
+ * Return: Pointer to the destination buffer.
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *final = dest;
+
+	while (*dest)
+		dest++;
+	while (*src)
+		*dest++ = *src++;
+	*dest = *src;
+	return (final);
+}
